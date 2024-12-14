017d3287671527a87a4e8404f525b0e4e5235983
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-12-14 22:18:18 +0600

    database context added, efc, server packages added

---
+++ your_bike_user_backend/Database/DatabaseContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using your_bike_user_backend.Models;
+
+namespace your_bike_user_backend.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+        public DbSet<Bike> Bikes { get; set; }
+    }
+}

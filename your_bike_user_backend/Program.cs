8640908f177a82dd3ded5750e1a536128db39d46
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-12-14 22:19:52 +0600

    db connection establishment

--- your_bike_user_backend/Program.cs
+++ your_bike_user_backend/Program.cs
@@ -1,8 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using your_bike_user_backend.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<DatabaseContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
+});
+builder.Services.AddControllers(option =>
+{
+    option.ReturnHttpNotAcceptable = true;
+}).AddXmlDataContractSerializerFormatters();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

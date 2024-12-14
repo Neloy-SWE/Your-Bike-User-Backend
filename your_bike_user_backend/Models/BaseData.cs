ca08fbc13a32a63e671b0fa8b4e67af75f98d4de
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-12-14 22:18:31 +0600

    base data model added

---
+++ your_bike_user_backend/Models/BaseData.cs
@@ -0,0 +1,9 @@
+﻿namespace your_bike_user_backend.Models
+{
+    public class BaseData<T>
+    {
+        public required string Status { get; set; }
+        public required string Message { get; set; }
+        public required T Data { get; set; }
+    }
+}

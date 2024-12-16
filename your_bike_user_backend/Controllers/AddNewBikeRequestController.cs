f791ff02e8d9376ed6d4629d44859abe6d8e2680
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-12-16 21:04:43 +0600

    add new bike request api created

---
+++ your_bike_user_backend/Controllers/AddNewBikeRequestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using your_bike_user_backend.Database;
+
+namespace your_bike_user_backend.Controllers
+{
+    [ApiController]
+    [Route("api/yourBike")]
+    public class AddNewBikeRequestController(DatabaseContext databaseContext) : ControllerBase
+    {
+        private readonly DatabaseContext _databaseContext = databaseContext;
+
+        [HttpPost("AddNewBikeRequest")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        public ActionResult<IEnumerable<>>
+    }
+}

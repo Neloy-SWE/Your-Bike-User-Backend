8b6ab0c56a62b2ff1ec6b25ebca019d51cc3271d
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-12-16 23:57:46 +0600

    add new bike request controller implemented

--- your_bike_user_backend/Controllers/AddNewBikeRequestController.cs
+++ your_bike_user_backend/Controllers/AddNewBikeRequestController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using your_bike_user_backend.Database;
+using your_bike_user_backend.Models;
 
 namespace your_bike_user_backend.Controllers
 {
@@ -10,8 +11,60 @@ namespace your_bike_user_backend.Controllers
         private readonly DatabaseContext _databaseContext = databaseContext;
 
         [HttpPost("AddNewBikeRequest")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [ProducesResponseType(StatusCodes.Status202Accepted)]
-        public ActionResult<IEnumerable<>>
+        public IActionResult AddNewBikeRequest([FromBody] Notification notification)
+        {
+            try
+            {
+                if (_databaseContext.Bikes.FirstOrDefault(u => u.Name.ToLower() == notification.Model.ToLower()) != null)
+                {
+                    BaseData<String> fail = new()
+                    {
+                        Status = "fail",
+                        Message = "Bike already Exists!",
+                        Data = ""
+                    };
+                    return BadRequest(fail);
+                }
+                if (notification == null)
+                {
+                    BaseData<String> fail = new()
+                    {
+                        Status = "fail",
+                        Message = "Did not get any data from user-end!",
+                        Data = ""
+                    };
+                    return BadRequest(fail);
+                }
+
+                Notification newNotification = new() { Brand = notification.Brand, Model = notification.Model, Read = false };
+
+                _databaseContext.Notifications.Add(newNotification);
+                _databaseContext.SaveChanges();
+
+                BaseData<String> success = new()
+                {
+                    Status = "success",
+                    Message = "Request added!",
+                    Data = "An admin will look into this very soon."
+                };
+
+                return CreatedAtAction(nameof(AddNewBikeRequest), success);
+
+            }
+            catch (Exception)
+            {
+                BaseData<string> data = new()
+                {
+                    Status = "fail",
+                    Message = "Something went wrong!",
+                    Data = "Try again leter."
+                };
+                return Accepted(data);
+            }
+        }
     }
 }

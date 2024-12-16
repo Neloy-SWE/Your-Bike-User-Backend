f4bef32294e4b23dfb0d255d3112e439c85c64f9
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-12-16 21:04:25 +0600

    add new notification model added

---
+++ your_bike_user_backend/Models/Notification.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace your_bike_user_backend.Models
+{
+    public class Notification
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public required string Brand { get; set; }
+        public required string Model { get; set; }
+    }
+}
